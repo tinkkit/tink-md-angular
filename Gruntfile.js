9329cf63642dede2ef2b9d068c1efd2f1d3ab575
Jasper Van Proeyen <trianglejuice@mac.com>
2015-06-03 14:27:27 +0200

    Ready for release

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
       ' * <%= pkg.description %>\n' +
       ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
       ' * @link <%= pkg.homepage %>\n' +
-      ' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n' +
+      // ' * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n' +
       ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
       ' */\n'
     },
@@ -149,7 +149,7 @@ module.exports = function (grunt) {
 
   // Build task.
   grunt.registerTask('build', [
-    // 'test',
+    'test',
     'concat',
     'ngmin',
     'uglify',
